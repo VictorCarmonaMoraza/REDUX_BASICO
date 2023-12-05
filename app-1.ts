b3f3ab442f2ecb4f8e72732a43aa77fbf47ca540
Victor <VictorCarmona9035@hotmail.com>
2023-12-05 07:46:58 +0100

    17-Acciones y Reducer basico

--- app-1.ts
+++ app-1.ts
@@ -1,1 +1,26 @@
-console.log('Hola Mundo');
+
+//Acciones
+interface Action {
+    type: string;
+    payload?: any;     //--> Son los parametros que nosotros le enviamos a la accion
+}
+
+//Utilizar la accion
+//El reducer recibe dos parametros 
+//state  --> estado anterior
+//action --> Es la accion que nosotros queremos que realice
+function reducer(state = 10, action: Action) {
+    if (action.type === 'INCREMENTAR') {
+        return state += 1;
+    }
+    return state;
+}
+
+//Utilizar la accion
+const incrementadorAction: Action = {
+    type: 'INCREMENTAR'
+};
+
+//Usar el reducer
+let valorReducer = reducer(10, incrementadorAction);
+console.log(valorReducer);
